82203ff1a30baa7cf5f30a2e4327cef76d3fb94b
incodi404 <dipankarnn2123@gmail.com>
2024-07-28 21:16:56 +0530

    oo

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(cookieParser())
 app.use(cookieSession({
     name: 'colsession',
     keys: [process.env.SESSION_KEY_ONE, process.env.SESSION_KEY_TWO],
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     sameSite: "none",
     maxAge: 24*60*60*1000
