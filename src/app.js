6c9965edc7a660e3777f789dd66586d97a4a75ef
incodi404 <dipankarnn2123@gmail.com>
2024-07-28 17:50:28 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cookieSession({
     name: 'colsession',
     keys: [process.env.SESSION_KEY_ONE, process.env.SESSION_KEY_TWO],
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "none",
     maxAge: 24*60*60*1000
 }))
