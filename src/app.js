6e611149ab49c05a398c5734349ee463a89cd44d
incodi404 <dipankarnn2123@gmail.com>
2024-07-28 21:53:47 +0530

    s

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ app.use(cookieSession({
     keys: [process.env.SESSION_KEY_ONE, process.env.SESSION_KEY_TWO],
     httpOnly: true,
     secure:true,
+    sameSite: "none",
     maxAge: 24*60*60*1000
 }))
 app.use(cookieParser())
